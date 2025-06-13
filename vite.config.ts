db308fb1ac98af6db668b3a6234ec477def98e56
Abhishek Reddy S <118092484+AbhishekReddys07@users.noreply.github.com>
2025-06-13 19:03:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/EmployeeDashboard/', // 👈 this should match your repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
+
